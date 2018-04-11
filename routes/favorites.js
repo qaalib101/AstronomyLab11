92e532c7e7483e85c830e70e2cf6e74094f78455
qaalib101 <si8822fb@go.minneapolis.edu>
2018-04-10 19:32:04 -0500

    Initial commit

---
+++ routes/favorites.js
@@ -0,0 +1,35 @@
+/**
+ * Created by si8822fb on 4/10/2018.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET favorites page */
+router.get('/', function(req, res, next){
+    res.render('favorites', { favorites: req.session.favorites });
+});
+
+/* POST to add a new favorite to user's favorites */
+router.post('/add', function(req, res, next){
+
+    // If a favorites array does not exist in this session, create it
+    if (!req.session.favorites) {
+        req.session.favorites = [];
+    }
+
+    // Is this image already a favorite? Filter the favorites array for images with this date
+    var favorite_on_date = req.session.favorites.filter(function(fav) {
+        return fav.date == req.body.date
+    });
+
+    // If no favorite found with this date, then add to the session's favorites array
+    if (favorite_on_date.length == 0) {
+        req.session.favorites.push(req.body);
+    }
+
+    // Redirect to the favorites page.
+    res.redirect('/favorites');
+});
+
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var apodService = require('../services/apod');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.redirect('fetchpicture');
+});
+
+
+/* Fetch a picture from NASA's Astronomy Picture of the Day service */
+router.get('/fetchpicture', function(req, res, next){
+
+    // If random picture requested, fetch random picture.
+    // Otherwise fetch today's picture.
+
+    apodService(function(err, apod_data){
+
+        if (err) {
+            res.render('apod_error', { message: err.message, title: 'Error' });
+        } else {
+            res.render('index', {apod: apod_data, title: `APOD for ${apod_data.date}`});
+        }
+
+    }, req.query.picturetype);
+
+});
+
+module.exports = router;
